bae58ba733f32428cd1df712c83de8ed5acc2c13
Pardeshi-Aditya <mailadityapardeshi@gmail.com>
2024-08-06 02:17:11 +0530

    chore : update packages and

--- x10store/tailwind.config.js
+++ x10store/tailwind.config.js
@@ -38,6 +38,9 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      fontFamily: {
+        sans: ["Montserrat", 'sans-serif'],
+    },
     },
   },
   plugins: [require('tailwindcss-animate')],
