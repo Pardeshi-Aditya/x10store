d6f6db5e026fbd16ef12e3e0ba11f5690f11f7d9
Pardeshi-Aditya <mailadityapardeshi@gmail.com>
2024-08-05 17:48:02 +0530

    fix : update size of logo

--- x10store/src/assets/images.tsx
+++ x10store/src/assets/images.tsx
@@ -1,3 +1,3 @@
 import logo from "./logo.png"
 
-export const LogoImage = () => <img src={logo} className="w-auto h-[100px] object-cover "></img>
+export const LogoImage = () => <img src={logo} className="h-[200px] w-auto object-cover"></img>
