e2acce52ff6079ec80673e7d0c6a4bd7c5309c89
Pardeshi-Aditya <mailadityapardeshi@gmail.com>
2024-08-12 15:08:42 +0530

    UI : add routes to different containers info pages.

--- x10store/src/App.tsx
+++ x10store/src/App.tsx
@@ -4,6 +4,8 @@ import { Theme } from '@radix-ui/themes';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ErrorPage from './components/ErrorPage';
+import LunchboxModels from './components/LunchboxModels';
+import ContainerModels from './components/ContainerModels';
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="*" element={<ErrorPage />} />
+              <Route path="/lunchbox" element={<LunchboxModels />} />
+              <Route path="/container" element={<ContainerModels />} />
             </Routes>
           </Router>
         </FrappeProvider>
